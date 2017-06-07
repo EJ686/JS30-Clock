8b59f6b0b059cc987be9e6ff94ceeb80cbabebec
Edgar Juarez <ejjt.686@gmail.com>
2017-06-06 18:47:41 -0700

    Clock hands now work for seconds,minute and hour

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform= `rotate(${secondsDegrees}deg)`;
@@ -11,8 +12,10 @@ function setDate() {
 
     const minute = now.getMinutes();
     const minuteDegree = ((minute / 60) * 360) + 90;
-    minuteHand.style.transform = `rotate(${minute}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
 
-    const
+    const hour = now.getMinutes();
+    const hourDegree = ((minute / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
 }
 setInterval(setDate, 1000);
