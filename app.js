641aee385d69c40185b1842e10ca94b05ae4c31b
Edgar Juarez <ejjt.686@gmail.com>
2017-06-06 18:39:57 -0700

    Clock minute hand now moves for every minute

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
@@ -6,5 +8,11 @@ function setDate() {
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform= `rotate(${secondsDegrees}deg)`;
     console.log(seconds);
+
+    const minute = now.getMinutes();
+    const minuteDegree = ((minute / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minute}deg)`;
+
+    const
 }
 setInterval(setDate, 1000);
